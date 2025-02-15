d98d4ceee647fc6f34a084e447ba71380d9b8601
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 05:32:15 -0600

    Ejercicio 3

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+double numDouble = 9.87;
+int numEntero = (int)numDouble; // Conversión de double a int (pierde decimales)
+
+int otroEntero = 42;
+double otroDouble = (double)otroEntero; // Conversión de int a double
+
+Console.WriteLine("Double convertido a int (pierde decimales): " + numEntero);
+Console.WriteLine("Int convertido a double: " + otroDouble);
+
+Console.WriteLine("El Ing Daro es muy feo.");
+
+Console.WriteLine("Pero buena onda");
